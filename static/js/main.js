d252add11c664937d09948a686bd9ec5992795c5
Howard Osborne <howardosborneconsulting@gmail.com>
2023-10-28 17:54:51 +0100

    stuff

--- static/js/main.js
+++ static/js/main.js
@@ -257,8 +257,8 @@ function get_hops(id){
     var arr = JSON.parse(this.responseText);
     for(i = 0; i < arr.length; i++) {
       if(arr[i].place_longer_desc.length > 0){
-        var marker = L.circle([arr[i].to_lat, arr[i].to_lon], {color: '#FF7933',fillColor: '#FF7933',fillOpacity: 0.5,radius: 10000});
-        marker.bindTooltip(arr[i].to_place_name);
+        var marker = L.circle([arr[i].place_lat, arr[i].place_lon], {color: '#FF7933',fillColor: '#FF7933',fillOpacity: 0.5,radius: 10000});
+        marker.bindTooltip(arr[i].place_name);
         marker.properties = arr[i];
         marker.addEventListener('click', _markerOnClick);
         marker.addTo(possible_hops)
