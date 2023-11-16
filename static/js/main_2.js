908ec62e20134a546260171a4871e2d84c17dc6d
howardosborne <hosborne@hotmail.com>
2023-11-16 15:47:36 +0000

    stuff

--- static/js/main_2.js
+++ static/js/main_2.js
@@ -22,7 +22,12 @@ function start(){
     //add the various layers to be used
     possible_start_points = new L.LayerGroup();
     map.addLayer(possible_start_points);
-  
+    var sidebar = L.control.sidebar({
+      autopan: false,       // whether to maintain the centered map point when opening the sidebar
+      closeButton: true,    // whether t add a close button to the panes
+      container: 'sidebar', // the DOM container or #ID of a predefined sidebar container that should be used
+      position: 'left',     // left or right
+  }).addTo(map);
     possible_hops = new L.LayerGroup();
     map.addLayer(possible_hops);
   
