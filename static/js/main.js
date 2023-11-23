946b6b059eb4fff856f7fc506caf0fc50425db69
howardosborne <hosborne@hotmail.com>
2023-11-23 16:29:42 +0000

    stuff

--- static/js/main.js
+++ static/js/main.js
@@ -24,10 +24,11 @@ function start(){
     //make a map
     map = L.map('map').setView([45, 10], 5);
     const tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {maxZoom: 19,attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'}).addTo(map);
-    L.easyButton('<img class="fa-regular fa-lightbulb">', function(btn, map){
+    L.easyButton('<img src="./static/icons/lightbulb.png">', function(btn, map){
       open_offcanvas('offcanvasInspire')
     }).addTo(map);
-    L.easyButton('<img src="./static/icons/triphop.png">', function(btn, map){
+    //var my_icon = L.icon({iconUrl: `./static/icons/triphop.png`,iconSize: [28, 28]});
+    L.easyButton('<img src="./static/icons/triphop_icon.png">', function(btn, map){
       buildAccordion();
       open_offcanvas("offcanvasTrip");
     }).addTo(map);
