e52612018ee18d190aedaa54869f17d41cb1ea39
howardosborne <hosborne@hotmail.com>
2023-12-20 11:25:52 +0000

    stuff

--- static/js/main.js
+++ static/js/main.js
@@ -104,7 +104,7 @@ function start(){
 
 function get_start_points(){
   var url = "./static/places.json";
-  var otherPlacesColour = getElementById("otherPlacesColour").value
+  var otherPlacesColour = document.getElementById("otherPlacesColour").value
   var xmlhttp = new XMLHttpRequest();
   xmlhttp.onreadystatechange = function() {
   if (this.readyState == 4 && this.status == 200) {
