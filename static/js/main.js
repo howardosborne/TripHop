0e596edd3210d10d508146464d66fb2dae9eca24
howardosborne <hosborne@hotmail.com>
2023-12-20 21:19:23 +0000

    stuff

--- static/js/main.js
+++ static/js/main.js
@@ -179,7 +179,7 @@ function show_start_message(){
     <div class="card-body">
       <h5 class="card-title">Plan your next trip - one hop at a time</h5>
       <p class="card-text">Pick a place and see where you can go in a single hop - stay for as little or long as you like and move on.</p>
-      <img src="./static/icons/inspireme.png" onclick="open_offcanvas('offcanvasInspire')">
+      <a class="icon-link" href="#" onclick="open_offcanvas('offcanvasInspire')">Inspire me! <svg class="bi" aria-hidden="true"><use xlink:href="#arrow-right"></use></svg></a>      
     </div>
   </div> 
 `
